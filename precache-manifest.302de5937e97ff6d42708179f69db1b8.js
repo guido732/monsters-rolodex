6e29befdd09e3eec8811bda84c80c6f5add51c9a
guido732 <guido732@gmail.com>
2019-11-25 11:53:59 -0300

    Updates

---
+++ precache-manifest.302de5937e97ff6d42708179f69db1b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874e61fc827f77555f67d20a5a7ba676",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0860d88160273cd1bf6a",
+    "url": "/monsters-rolodex/static/css/main.e50870a0.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "0860d88160273cd1bf6a",
+    "url": "/monsters-rolodex/static/js/main.10c3ce4e.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
